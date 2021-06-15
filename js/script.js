d96df64d5383111e41d5c719fe55dacfd95b8856
Ichoi1029 <78829722+Ichoi1029@users.noreply.github.com>
2021-06-15 18:14:43 +0900

    bug 수정, 코드 정리, 서비스 페이지 작성

--- js/script.js
+++ js/script.js
@@ -2,18 +2,17 @@
 
 $(document).ready(function(){
     // basic slider
-    $('.main__banner').slick({
-            dots:true,
-            autoplay: true,
-            autoplaySpeed: 2000,
-    });
+        $('.main__banner').slick({
+                dots:true,
+                autoplay: true,
+                autoplaySpeed: 2000,
+        });
     // responsive
-    $('.responsive__slider').slick({
-            dots: true,
-            infinite: true,
-            speed: 300,
-            slidesToShow: 3,
-            slidesToScroll: 3,
-    });
-
+        $('.responsive__slider').slick({
+                dots: true,
+                infinite: true,
+                speed: 300,
+                slidesToShow: 4,
+                slidesToScroll: 4,
+        });
 });
