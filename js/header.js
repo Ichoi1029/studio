c641d12a6ea10e60321af399a42939ba9872161b
Ichoi1029 <78829722+Ichoi1029@users.noreply.github.com>
2021-06-17 18:52:35 +0900

    js -> jq

--- js/header.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.gnb-toggle-btn');
-const gnb = document.querySelector('.gnb');
-
-toggleBtn.addEventListener('click', () => {
-    gnb.classList.toggle('active');
-});
--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 'use strict';
+/* ========================= header ========================= */
+$(function(){
+        var $toggle = $('.gnb-toggle-btn'),
+        $gnb = $('.gnb');
 
+        $($toggle).click(function(){
+        $(this).toggleClass('active');
+        $($gnb).toggleClass('active');
+        })
+})
+/* ========================= main banner ========================= */
 $(document).ready(function(){
         $('.main__banner').slick({
                 dots:true,
