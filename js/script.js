06c716cb8480de3e65b99e59ed0b2aa755bbe294
Ichoi1029 <78829722+Ichoi1029@users.noreply.github.com>
2021-06-19 10:12:33 +0900

    0619 최신소스 커밋

--- js/script.js
+++ js/script.js
@@ -4,16 +4,17 @@ $(function(){
         var $toggle = $('.gnb-toggle-btn'),
         $gnb = $('.gnb');
 
-        $($toggle).click(function(){
+        $toggle.click(function(){
         $(this).toggleClass('active');
-        $($gnb).toggleClass('active');
+        $gnb.toggleClass('active');
         })
-})
 /* ========================= main banner ========================= */
-$(document).ready(function(){
-        $('.main__banner').slick({
-                dots:true,
-                autoplay: true,
-                autoplaySpeed: 2000,
+        $(document).ready(function(){
+                $('.main__banner').slick({
+                        dots:true,
+                        autoplay: true,
+                        autoplaySpeed: 2000,
+                });
         });
-});
+})
+
