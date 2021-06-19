d4a201e6c4659b50951366775fe927e05eb4d8be
Ichoi1029 <78829722+Ichoi1029@users.noreply.github.com>
2021-06-19 11:29:54 +0900

    gnb bug fix 전

--- js/script.js
+++ js/script.js
@@ -9,12 +9,49 @@ $(function(){
         $gnb.toggleClass('active');
         })
 /* ========================= main banner ========================= */
-        $(document).ready(function(){
-                $('.main__banner').slick({
-                        dots:true,
-                        autoplay: true,
-                        autoplaySpeed: 2000,
-                });
-        });
+        // main slider
+    $('.main__banner').slick({
+        dots:true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+});
+
+// responsive
+$('.responsive__slider').slick({
+        dots: true,
+        infinite: true,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+            },
+            {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+            },
+            {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+            }
+        ]
+});
+
+        
 })
 
